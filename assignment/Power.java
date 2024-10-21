171894a329fd007bbc16122adc1f06f38180354c
xtianah124 <oludechristianah16@email.com>
2024-10-21 08:26:04 +0100

    assignment

---
+++ assignment/Power.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class Power{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+for(int vary = 0; vary <= 5; vary = vary +2){
+System.out.println(vary);
+    }
+
+
+  }
+
+
+}
