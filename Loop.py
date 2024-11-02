f0043ac3625157bc43864b79548ac546b71f3ee6
xtianah124 <oludechristianah16@email.com>
2024-11-02 17:58:32 +0100

    multiply

---
+++ Loop.py
@@ -0,0 +1,18 @@
+number = int(input("Enter a number"))
+
+for(int i = 1;i < 5;i++):
+
+    for (int j = 4;j > i;j--):
+	print(" ")
+
+    for (int m = 2;m <= i;m++):
+	print(m)
+    	
+    for (int k = 1;k <= i;k++):
+	print(k)
+    		
+    for (int j = 5;j > i;j--):
+	print(" ")
+    
+
+	println(" ")
