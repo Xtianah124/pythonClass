84901794819ff186636029092bb3342928e66225
xtianah124 <oludechristianah16@email.com>
2024-11-02 17:55:05 +0100

    turing_test, print, column_and_row

---
+++ chapter 3/largest_smallest_numbers.py
@@ -0,0 +1,35 @@
+number1 = int(input("Enter a number"))
+number2 = int(input("Enter a number"))
+number3 = int(input("Enter a number"))
+number4 = int(input("Enter a number"))
+
+int sum = number1 + number2 + number3 + number4
+print(sum)
+
+
+int product = number1 * number2 * number3 * number4
+print(product)
+
+if (number1 > number2):
+print("number1 is the largest number")
+
+elif (number2 > number3):
+print("number2 is the largest number")
+
+elif (number3 > number4):
+print("number3 is the largest number")
+
+
+
+if (number1 < number2):
+print("number1 is the smallest number")
+
+elif (number2 < number3):
+print("number2 is the smallest number")
+
+elif (number3 < number4):
+print("number3 is the smallest number")
+
+
+
+
