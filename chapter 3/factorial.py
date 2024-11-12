6fd3b31d79f712aab24ba6111624c27dab6b1b16
xtianah124 <oludechristianah16@email.com>
2024-11-12 06:32:29 +0100

    8-11-24

--- chapter 3/factorial.py
+++ chapter 3/factorial.py
@@ -1,7 +1,7 @@
 number = int(input("Enter number"))
 factorial = 1
 
-while (number < 5):
+while (number <=  5):
 	
     factorial = factorial * number
     number = number -1
---
+++ num.py
@@ -0,0 +1,4 @@
+for number in range(1, 11):
+	for num in range(1, number+1):
+		print(num,end = '  ')
+	print()
